d83498331f639c4c89a246d806c7033953d14aa2
Giovanni Rebouças <giovannireboucas@hotmail.com>
2026-05-25 01:12:55 -0300

    Removed seasonal NPCs from map location. Removed non-cannon NPCs. Fixed some monsters that had problematic loot and area effects. Implemented Seasonal Events system (still untested) (#315).

--- data/globalevents/scripts/events/seasonalEvents.lua
+++ data/globalevents/scripts/events/seasonalEvents.lua
@@ -1,11 +1,80 @@
+CityRaidAreas = {  -- Define standard areas for raids in cities
+    Svargrond = {
+        topLeftPos = {x="32325", y="32184", z="7"},
+        bottomRightPos = {x="32432", y="32270", z="7"}
+    },
+    Yalahar = {
+        topLeftPos = {x="32325", y="32184", z="7"},
+        bottomRightPos = {x="32432", y="32270", z="7"}
+    },
+    Carlin = {
+        topLeftPos = {x="32325", y="32184", z="7"},
+        bottomRightPos = {x="32432", y="32270", z="7"}
+    },
+    AbDendriel = {
+        topLeftPos = {x="32325", y="32184", z="7"},
+        bottomRightPos = {x="32432", y="32270", z="7"}
+    },
+    Kazordoon = {
+        topLeftPos = {x="32325", y="32184", z="7"},
+        bottomRightPos = {x="32432", y="32270", z="7"}
+    },
+    Edron = {
+        topLeftPos = {x="32325", y="32184", z="7"},
+        bottomRightPos = {x="32432", y="32270", z="7"}
+    },
+    Thais = {
+        topLeftPos = {x="32325", y="32184", z="7"},
+        bottomRightPos = {x="32432", y="32270", z="7"}
+    },
+    Venore = {
+        topLeftPos = {x="32325", y="32184", z="7"},
+        bottomRightPos = {x="32432", y="32270", z="7"}
+    },
+    Darashia = {
+        topLeftPos = {x="32325", y="32184", z="7"},
+        bottomRightPos = {x="32432", y="32270", z="7"}
+    },
+    LibertyBay = {
+        topLeftPos = {x="32325", y="32184", z="7"},
+        bottomRightPos = {x="32432", y="32270", z="7"}
+    },
+    PortHope = {
+        topLeftPos = {x="32325", y="32184", z="7"},
+        bottomRightPos = {x="32432", y="32270", z="7"}
+    },
+    Ankrahmun = {
+        topLeftPos = {x="32325", y="32184", z="7"},
+        bottomRightPos = {x="32432", y="32270", z="7"}
+    },
+}
+
+DryadRaidConfig = {
+    Edron = {
+        message = "Dryads have returned to protect the forest north of Edron.",
+        topLeftPos = {x="33156", y="31683", z="7"},
+        bottomRightPos = {x="33208", y="31726", z="7"}
+    },
+    PortHope = {
+        message = "Dryads have returned to protect the jungle of Tiquanda.",
+        topLeftPos = {x="32733", y="32691", z="7"},
+        bottomRightPos = {x="32792", y="32752", z="7"}
+    },
+    Carlin = {
+        message = "Dryads have returned to protect the northern forests near Ab'Dendriel.",
+        topLeftPos = {x="32418", y="31693", z="7"},
+        bottomRightPos = {x="32469", y="31742", z="7"}
+    }
+}
+
 SeasonalEvents = {
     -- Thais Primitive Raid during Tibia's Anniversary month
     ThaisPrimitiveRaid = {
-        schedule = "Daily",
+        schedule = "Random",
         period = {
             startMonth = 1,  -- January
             startDay = 15,
-            endMonth = 6,  -- February
+            endMonth = 2,  -- February
             endDay = 15
         },
         type = "raid",
@@ -17,8 +86,8 @@ SeasonalEvents = {
                         {name="Hacker", amount="40"},
                         {name="Tibia Bug", amount="40"}
                     },
-                    topLeftPos = {x="32325", y="32184", z="7"},
-                    bottomRightPos = {x="32432", y="32270", z="7"},
+                    topLeftPos = CityRaidAreas.Thais.topLeftPos,
+                    bottomRightPos = CityRaidAreas.Thais.bottomRightPos,
                     delay = 1000,
                     message = "Primitives are attacking Thais!"
                 },
@@ -28,8 +97,8 @@ SeasonalEvents = {
                         {name="Hacker", amount="80"},
                         {name="Tibia Bug", amount="80"}
                     },
-                    topLeftPos = {x="32325", y="32184", z="7"},
-                    bottomRightPos = {x="32432", y="32270", z="7"},
+                    topLeftPos = CityRaidAreas.Thais.topLeftPos,
+                    bottomRightPos = CityRaidAreas.Thais.bottomRightPos,
                     delay = 120 * 1000,
                     message = "Primitives are everywhere in Thais trying a take over!"
                 },
@@ -39,17 +108,51 @@ SeasonalEvents = {
                         {name="Hacker", amount="110"},
                         {name="Tibia Bug", amount="110"}
                     },
-                    topLeftPos = {x = "32325", y = "32184", z = "7"},
-                    bottomRightPos = {x="32432", y="32270", z="7"},
+                    topLeftPos = CityRaidAreas.Thais.topLeftPos,
+                    bottomRightPos = CityRaidAreas.Thais.bottomRightPos,
                     delay = 240 * 1000,
                     message = "Stop the primitives in Thais!"
                 }
             }
         }
     },
+    -- Spawn Stan, NPC who sells costume bags, in Venore
+    MasqueradeDay = {
+        schedule = "Always",
+        period = {
+            startMonth = 2,  -- February
+            startDay = 1,
+            endMonth = 2,  -- February
+            endDay = 28
+        },
+        type = "npc"
+        content = {
+            npc = "Stan"
+            pos = {
+                {x="32950", y="32106", z="6"} -- Venore (+1)
+            }
+        }
+    },
+    -- Spawn Valentina, NPC who sells Valentine's Day items, in Greenshore
+    ValentinesDay = {
+        schedule = "Always",
+        period = {
+            startMonth = 2,  -- February
+            startDay = 14,
+            endMonth = 2,  -- February
+            endDay = 14
+        },
+        type = "npc"
+        content = {
+            npc = "Valentina"
+            pos = {
+                {x="32271", y="32052", z="7"} -- Greenshore (0)
+            }
+        }
+    },
     -- The Ruthless Herald raid during April Fools in PoH
     PoHAprilFoolsRaid = {
-        schedule = "Daily",
+        schedule = "Random",
         period = {
             startMonth = 4,  -- April
             startDay = 1,
@@ -82,6 +185,254 @@ SeasonalEvents = {
             }
         }
     },
+    -- Spawn Hoaxette, NPC who trades pieces of Jester Dolls, in Thais
+    AprilFoolsDay = {
+        schedule = "Always",
+        period = {
+            startMonth = 4,  -- April
+            startDay = 1,
+            endMonth = 4,  -- April
+            endDay = 30
+        },
+        type = "npc"
+        content = {
+            npc = "Hoaxette"
+            pos = {
+                {x="32369", y="32214", z="7"} -- Thais (0)
+            }
+        }
+    },
+    UndeadJesterRaid = {
+        schedule = "Random",
+        period = {
+            startMonth = 4,  -- April
+            startDay = 1,
+            endMonth = 4,  -- April
+            endDay = 15
+        },
+        type = "globalraid",  -- Global Raids are raids that can happen in different cities, at random
+        content = {
+            areaSpawns = {  -- Where the area spawns will happen and when and what message will be sent
+                {
+                    monsters = {
+                        {name="Undead Jester", amount="300"}
+                    },
+                    delay = 1000,
+                    message = "Oh no! |CITY_NAME| beware! All bad entertainers are returning from their graves to haunt their audience once again!"
+                }
+            }
+        }
+    },
+    HalloweenHareRaid = {
+        schedule = "Random",
+        period = {
+            startMonth = 10,  -- October
+            startDay = 31,
+            endMonth = 10,  -- October
+            endDay = 31
+        },
+        type = "raid",
+        content = {
+            announcements = {  -- Only announcements, no spawns here
+                {
+                    message = "Beware, beware the halloween hare.",
+                    delay = 1000
+                }
+            },
+            singleSpawns = {  -- Where the singles spawns will happen and when and what message will be sent (message is optional)
+                {    
+                    monster = "The Halloween Hare",
+                    pos = {x="32615", y="31983", z="7"},  -- South of Dwarven Bridge
+                    delay = 1000
+                },
+                {    
+                    monster = "The Halloween Hare",
+                    pos = {x="32097", y="32202", z="7"},  -- Rookgaard center
+                    delay = 1000
+                }
+            }
+        }
+    },
+    MutatedPumpkinRaid = {
+        schedule = "Random",
+        period = {
+            startMonth = 10,  -- October
+            startDay = 31,
+            endMonth = 11,  -- November
+            endDay = 3
+        },
+        type = "raid",
+        content = {
+            singleSpawns = {  -- Where the singles spawns will happen and when and what message will be sent (message is optional)
+                {    
+                    monster = "The Mutated Pumpkin",
+                    pos = {x="33170" y="32433" z="7"},
+                    delay = 1000,
+                    message = "Oh noes! It's a mutated pumpkin!"
+                }
+            }
+        }
+    },
+    Christmas = {
+        schedule = "Always",
+        period = {
+            startMonth = 12,  -- December
+            startDay = 12,
+            endMonth = 12,  -- December
+            endDay = 31
+        },
+        type = "npc"
+        content = {
+            npc = "Santa Claus"
+            pos = {
+                {x="32655", y="31664", z="8"},  -- Ab'Dendriel (-1)
+                {x="33067", y="32880", z="6"},  -- Ankrahmun (+1)
+                {x="32313", y="31839", z="8"},  -- Carlin (-1)
+                {x="33232", y="32486", z="7"},  -- Darashia (0)
+                {x="33191", y="31810", z="7"},  -- Edron (0)
+                {x="33000", y="31538", z="10"},  -- Farmine (-3, fully restored city)
+                {x="32642", y="31894", z="9"},  -- Kazordoon (-2)
+                {x="32322", y="32834", z="7"},  -- Liberty Bay (0)
+                {x="32623", y="32751", z="7"},  -- Port Hope (0)
+                {x="32240", y="31139", z="7"},  -- Svargrond (0)
+                {x="32362", y="32207", z="7"},  -- Thais (0)
+                {x="32915", y="32071", z="9"},  -- Venore (-2)
+                {x="32811", y="31245", z="7"}  -- Yalahar (0)
+            }
+        }
+    },
+    GrynchClanGoblinRaid = {
+        schedule = "Random",
+        period = {
+            startMonth = 12,  -- December
+            startDay = 20,
+            endMonth = 12,  -- December
+            endDay = 28
+        },
+        type = "globalraid",  -- Global Raids are raids that can happen in different cities, at random
+        content = {
+            areaSpawns = {  -- Where the area spawns will happen and when and what message will be sent
+                {
+                    monsters = {
+                        {name="Grynch Clan Goblin", amount="100"}
+                    },
+                    delay = 1000,
+                    message = "Goblins of the infamous Grynch Clan are invading |CITY_NAME| to steal all presents, beware!"
+                },
+                {
+                    monsters = {
+                        {name="Grynch Clan Goblin", amount="100"}
+                    },
+                    delay = 120 * 1000,
+                    message = "The Goblins sometimes have stolen presents with them! Confiscate them!"
+                },
+                {
+                    monsters = {
+                        {name="Grynch Clan Goblin", amount="100"}
+                    },
+                    delay = 180 * 1000,
+                    message = "Return the stolen presents to Ruprecht on Vega at Santa's home for a reward."
+                }
+            }
+        }
+    },
+    FlowerMonth = {
+        schedule = "Always",
+        period = {
+            startMonth = 6,  -- June
+            startDay = 1,
+            endMonth = 6,  -- June
+            endDay = 30
+        },
+        type = "npc"
+        content = {
+            npc = "Rosemarie"
+            pos = {
+                {x="32534", y="32827", z="7"}  -- Port Hope (0)
+            }
+        }
+    },
+    DryadRaid = {
+        schedule = "Random",
+        period = {
+            startMonth = 6,  -- June
+            startDay = 1,
+            endMonth = 6,  -- June
+            endDay = 30
+        },
+        type = "globalraid",  -- Global Raids are raids that can happen in different cities, at random
+        content = {
+            areaSpawns = {  -- Where the area spawns will happen and when and what message will be sent
+                {
+                    monsters = {
+                        {name="Dryad", amount="100"}
+                    },
+                    delay = 1000
+                },
+                {
+                    monsters = {
+                        {name="Dryad", amount="100"}
+                    },
+                    delay = 15 * 60 * 1000
+                },
+                {
+                    monsters = {
+                        {name="Dryad", amount="100"}
+                    },
+                    delay = 30 * 60 * 1000
+                },
+                {
+                    monsters = {
+                        {name="Dryad", amount="100"}
+                    },
+                    delay = 45 * 60 * 1000
+                },
+                {
+                    monsters = {
+                        {name="Dryad", amount="100"}
+                    },
+                    delay = 60 * 60 * 1000
+                }
+            }
+        }
+    },
+    HotCuisineQuest = {
+        schedule = "Always",
+        period = {
+            startMonth = 8,  -- August
+            startDay = 1,
+            endMonth = 8,  -- August
+            endDay = 31
+        },
+        type = "npc"
+        content = {
+            npc = "Jean Pierre"
+            pos = {
+                {x="33072", y="32528", z="6"}  -- Jean Pierre's House in Ashta'Daramai (+1)
+            }
+        }
+    },
+    NewYearsEve = {
+        schedule = "Always",
+        period = {
+            startMonth = 12,  -- December
+            startDay = 27,
+            endMonth = 1,  -- January
+            endDay = 3
+        },
+        type = "npc"
+        content = {
+            npc = "Ned Nobel"
+            pos = {
+                {x="32651", y="31700", z="7"},  -- Ab'Dendriel (0)
+                {x="32342", y="31795", z="7"},  -- Carlin (0)
+                {x="33191", y="31791", z="7"},  -- Edron (0)
+                {x="32610", y="31922", z="8"},  -- Kazordoon (-1)
+                {x="32321", y="32836", z="7"},  -- Liberty Bay (0)
+                {x="32329", y="32216", z="7"}  -- Thais (0)
+            }
+        }
+    }
 }
 
 RashidConfig = {
@@ -170,6 +521,8 @@ function onStartup()
             print("Seasonal Event: " .. name .. " in effect!")
             if param.type == "raid" then
                 addEvent(executeRaid, 60 * 1000, param.content)
+            elif param.type == "npc" then
+            elif param.type == "worldquest" then
             end
         end
     end
