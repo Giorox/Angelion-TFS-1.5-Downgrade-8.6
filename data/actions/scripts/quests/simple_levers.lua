fcc48f9cef8b545b93ad9ac293de1ac3f3e64798
Giovanni Rebouças <giovannireboucas@hotmail.com>
2026-08-08 01:01:12 -0300

    Lever System and Quests
    - Implemented "transform" action for Simple Lever System (SLS), allowing
      items to be transformed rather than only destroyed and created;
    - Re-implemented Katana Quest lever as a SLS entry;
    - Removed old Katana Quest Script;
    - Implemented MoLS hole lever;
    - Added `maxTextLen` attribute to parchment item (ID = 1953);
    - Implemented Purple Tome Quest (closes #210);
    - Implemented Griffin Shield Quest (closes #250);
    - Implemented Demona Ring Quest (closes #271);
    - Implemented Crystal Wand Quest (closes #277).

--- data/actions/scripts/quests/simple_levers.lua
+++ data/actions/scripts/quests/simple_levers.lua
@@ -1,7 +1,7 @@
 -- Simple Lever System by Giorox
 local levers = {
 	[2007] = {  -- Behemoth Quest Lever - Edron
-        id = 1304, 
+        id = 1304,  -- Big rock
         pos = {
             {x=33295, y=31677, z=15, stackpos=1},
             {x=33296, y=31677, z=15, stackpos=1},
@@ -10,6 +10,21 @@ local levers = {
             {x=33299, y=31677, z=15, stackpos=1}
         },
         effect = CONST_ME_HITAREA
+    },
+    [2008] = {  -- MoLS Lever
+        id = 383,  -- Hole
+        pos = {
+            {x=32483, y=31633, z=9, stackpos=1}
+        },
+        effect = CONST_ME_HITAREA,
+        action = "transform"
+    },
+    [5637] = { -- Katana Quest Lever - Rookgaard
+        id = 5109,  -- Open Door
+        pos = {
+            {x=32177, y=32148, z=11, stackpos=1}
+        },
+        action = "transform"
     },
 	[5638] = { -- Bear Room Lever - Rookgaard
         id = 1304,
@@ -21,7 +36,9 @@ local levers = {
 }
 
 local transformId = {
-    [1945] = 1946, [1946] = 1945 -- lever
+    [1945] = 1946, [1946] = 1945, -- lever
+    [383] = 9024, [9024] = 383, -- open shovel hole becomes cave floor
+    [5108] = 5109, [5109] = 5108, -- open door becomes closed door
 }
 
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
@@ -40,18 +57,31 @@ function onUse(player, item, fromPosition, target, toPosition, isHotkey)
     end
 
     local itemExists = Tile(target.pos[1]):getItemById(target.id)
+    local isTransform = target.action ~= nil
 
     if itemExists then
         for _, coord in pairs(target.pos) do
             local targetItem = Tile(coord):getItemById(target.id)
             if targetItem then
-                targetItem:remove()
-                Position(coord):sendMagicEffect(target.effect)
+                if isTransform then
+                    targetItem:transform(transformId[target.id])
+                else
+                    targetItem:remove()
+                end
+
+                if target.effect then
+                    Position(coord):sendMagicEffect(target.effect)
+                end
             end
         end
     else
         for _, coord in pairs(target.pos) do
-            Game.createItem(target.id, 1, coord)
+            if isTransform then
+                local targetItem = Tile(coord):getItemById(transformId[target.id])
+                targetItem:transform(target.id)
+            else
+                Game.createItem(target.id, 1, coord)
+            end
         end
     end
 
